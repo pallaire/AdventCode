724c6783a210076e9a081ca78978c4e052a18252
Patrick Allaire <patrick@pallaire.com>
2024-10-24 09:04:07 -0700

    2022 Day 10.2 Cathode ray tube

--- 2022/10/cathoderaytube.py
+++ 2022/10/cathoderaytube.py
@@ -1,11 +1,15 @@
 
-def problems(problem_id, lines):
+def problems(lines):
 
     cycle = 0
     X = 1
 
     dump_cycles = [20, 60, 100, 140, 180, 220]
     total = 0
+
+    stdout = []
+    for i in range(6):
+        stdout.append([' ' for x in range(40)])
 
     for aline in lines:
         tokens = aline.split()
@@ -15,22 +19,35 @@ def problems(problem_id, lines):
         else:
             arg = None
 
+        dest_line = int(cycle / 40)
+        pos_line = cycle % 40
+        if pos_line >= X-1 and pos_line <= X+1:
+            stdout[dest_line][pos_line] = '#'
+
+
         cycle += 1
         if cycle % 20 == 0:
             if cycle in dump_cycles:
                 total += cycle*X            
-                print(f"1. {command} Cycle={cycle} adding={cycle}x{X}, total={total}")
 
         if command == 'addx':
+            dest_line = int(cycle / 40)
+            pos_line = cycle % 40
+            if pos_line >= X-1 and pos_line <= X+1:
+                stdout[dest_line][pos_line] = '#'
+
             cycle += 1
             if cycle % 20 == 0:
                 if cycle in dump_cycles:
                     total += cycle*X            
-                    print(f"2. {command}@{arg} Cycle={cycle} adding={cycle}x{X}, total={total}")
             X += arg
             
         
-    print(f"Problem {problem_id} signal strength is {total}")
+    print(f"Problem 1 signal strength is {total}")
+    print(f"Problem 2 message is : ")
+    for s in stdout:
+        print(''.join(s))
+
 
 day = '10'
 # filename = f"day{day}test01.txt"
@@ -39,4 +56,4 @@ filename = f"day{day}data01.txt"
 lines = [aline.strip() for aline in open(filename).readlines()]
 
 print(f"2022 Day {day} using file [{filename}]")
-problems(1, lines)
+problems(lines)
