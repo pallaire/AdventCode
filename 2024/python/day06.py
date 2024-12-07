7e1b4e6ed2cbcf5d2b58a011e96432d1591d030f
Patrick Allaire <patrick@pallaire.com>
2024-12-07 11:50:28 -0800

    AC 2024 Day 06 use Pool for multithread, 5s to 500ms

--- 2024/python/day06.py
+++ 2024/python/day06.py
@@ -1,4 +1,6 @@
 from aoc import *
+from itertools import repeat
+from multiprocessing import Pool
 
 # up, right, down, left
 dirs = [(0,-1), (1,0), (0,1), (-1,0)]
@@ -29,6 +31,30 @@ def floorScanAndPath(lines):
 
     return (floormap, startpos, w, h, visited)
 
+def checkForLoop(obstruction, floormap, w, h, startpos):
+    if startpos == obstruction:
+        return 0
+    
+    guardpos = startpos
+    guarddir = 0
+    visited = {}
+    itsaloop = False
+    while(guardpos[0]>=0 and guardpos[0]<w and guardpos[1]>=0 and guardpos[1]<h):
+        if guardpos in visited:
+            if guarddir in visited[guardpos]:
+                return 1
+            else:
+                visited[guardpos].append(guarddir)
+        else:
+            visited[guardpos] = [guarddir]
+        destination = (guardpos[0] + dirs[guarddir][0], guardpos[1] + dirs[guarddir][1])
+        if destination in floormap or destination==obstruction:
+            guarddir = (guarddir+1)%4
+        else:
+            guardpos = destination
+    return 0
+
+
 def part1(lines):
     (floormap, startpos, w, h, visited) = floorScanAndPath(lines)
     return len(visited)
@@ -37,38 +63,8 @@ def part2(lines):
     (floormap, startpos, w, h, visited) = floorScanAndPath(lines)
 
     loops = 0
-    for obs in visited.keys():
-        if obs == startpos:
-            continue
-
-        # add temp obstruction
-        floormap.add(obs)
-
-        # Walk the new maze
-        guardpos = startpos
-        guarddir = 0
-        obsvisited = {}
-        itsaloop = False
-        while(guardpos[0]>=0 and guardpos[0]<w and guardpos[1]>=0 and guardpos[1]<h):
-            if guardpos in obsvisited:
-                if guarddir in obsvisited[guardpos]:
-                    itsaloop = True
-                    break
-                else:
-                    obsvisited[guardpos].append(guarddir)
-            else:
-                obsvisited[guardpos] = [guarddir]
-            destination = (guardpos[0] + dirs[guarddir][0], guardpos[1] + dirs[guarddir][1])
-            if destination in floormap:
-                guarddir = (guarddir+1)%4
-            else:
-                guardpos = destination
-
-        if itsaloop:
-            loops += 1
-
-        # remove temp obstruction
-        floormap.remove(obs)
+    with Pool() as pool:
+        loops = sum(pool.starmap(checkForLoop, zip(visited.keys(), repeat(floormap), repeat(w), repeat(h), repeat(startpos))))
 
     return loops
 
