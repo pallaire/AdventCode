85623c03076f591099714b4edbdc485e7a3bdbf4
Patrick Allaire <patrick@pallaire.com>
2025-11-21 13:21:50 -0800

    2021 Day 03.2

--- 2021/day03.cpp
+++ 2021/day03.cpp
@@ -7,6 +7,45 @@
 
 using namespace std;
 
+pair<string, string> filter(vector<string> lines, unsigned int pos) {
+  if(lines.size() == 1) {
+    return pair<string, string>(lines[0], lines[0]);    
+  }
+
+  if(pos >= 12) {
+    return pair<string, string>("","");    
+  }
+
+  if(lines.size() == 0) {
+    return pair<string, string>("","");    
+  }
+
+  vector<string> ones;
+  vector<string> zeros;
+
+  for(string s : lines)
+  {
+    if(s[pos] == '1') {
+      ones.push_back(s);
+    } else {
+      zeros.push_back(s);
+    }
+  }
+
+  pair<string, string> oxy;
+  pair<string, string> co2;
+  
+  if(ones.size() >= zeros.size()) {
+    oxy = filter(ones, pos+1);
+    co2 = filter(zeros, pos+1);
+  } else {
+    oxy = filter(zeros, pos+1);
+    co2 = filter(ones, pos+1);
+  }
+
+  return pair<string, string>(oxy.first, co2.second);
+}
+
 int main (int argc, char** argv) {
   cout << "Running Day number > " << DAY_NUM << std::endl;
   
@@ -16,6 +55,7 @@ int main (int argc, char** argv) {
 
   int bitcount = lines[0].size();
   array<unsigned int, 16> counter = {0};
+
 
   for(string l : lines) {
     for(int b = 0; b < bitcount; b++) {
@@ -43,6 +83,15 @@ int main (int argc, char** argv) {
   }
 
   cout << "Result 1 : " << (gamma*epsilon) << std::endl;
+
+
+
+  pair<string, string> filtered = filter(lines, 0);
+
+  unsigned long oxy = stoi(filtered.first, nullptr, 2);
+  unsigned long co2 = stoi(filtered.second, nullptr, 2);
+
+  cout << "Result 2 : " << (oxy * co2) << std::endl;
 
 
   return 0;
