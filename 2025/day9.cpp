e8280844f6d80345c1271346fb95156e2fed0b7e
Patrick Allaire <patrick@pallaire.com>
2025-12-12 11:45:02 -0800

    2025 Day 09 Part 2

--- 2025/day9.cpp
+++ 2025/day9.cpp
@@ -2,7 +2,6 @@
 #include <array>
 #include <vector>
 
-
 #include "p2dmap.h"
 #include "pchrono.h"
 #include "pfile.h"
@@ -17,7 +16,8 @@ int main(int argc, char** argv) {
     vector<u64> nums = file.getDataOfNumbers();
     u64 numcount = nums.size() / 2;
 
-    // u64 maxx, maxy;
+    i64 maxx = 0;
+    i64 maxy = 0;
 
     PChrono* p1timing = new PChrono("Problem1");
     u64 res = 0;
@@ -29,151 +29,158 @@ int main(int argc, char** argv) {
             i64 bx = nums[b*2];
             i64 by = nums[(b*2)+1];
 
+            rect++;
+
             area = (abs(ax-bx)+1) * (abs(ay-by)+1); // +1 because numbers are inclusive
             if(area > res) {
                 res = area;
             }
 
-            // if(ax > maxx) {
-            //     maxx = ax;
-            // }
+            if(ax > maxx) {
+                maxx = ax;
+            }
 
-            // if(ay > maxy) {
-            //     maxy = ay;
-            // }
+            if(ay > maxy) {
+                maxy = ay;
+            }
         }
     }
     cout << "Result 1 : " << res << std::endl;
     delete p1timing;
 
-
+    cout << "rect count : " << rect << std::endl;
 
     PChrono* p2timing = new PChrono("Problem2");
     res = 0;
 
-    // Same approach, but search for pair that would be inside the 
-    // current area
-    area = 0;
-    for(u64 a = 0; a <= numcount; a++) {
+    maxx++;
+    maxy++;
+
+    PChrono* p3timing = new PChrono("Alloc and init");
+    const u64 cachesize = 100000;
+    i32* left = new i32[cachesize];
+    i32* right = new i32[cachesize];
+    i32* top = new i32[cachesize];
+    i32* bottom = new i32[cachesize];
+    for(u64 i = 0; i < cachesize; i++) {
+        left[i] = 1000000;
+        right[i] = -1;
+        top[i] = 1000000;
+        bottom[i] = -1;
+    }
+
+    // find the borders
+    for(u64 a = 0; a < numcount; a++) {
+        u64 b = (a+1)%numcount;
+        i64 ax = nums[a*2];
+        i64 ay = nums[(a*2)+1];
+        i64 bx = nums[b*2];
+        i64 by = nums[(b*2)+1];
+
+        if(ax > bx) {
+            i64 tmp = bx;
+            bx = ax;
+            ax = tmp;
+        }
+
+        if(ay > by) {
+            i64 tmp = by;
+            by = ay;
+            ay = tmp;
+        }
+
+        if(ax == bx) {
+            //vertical
+            for(i64 y = ay; y <= by; y++) {
+                if(ax < left[y]) {
+                    left[y] = ax;
+                }
+
+                if(ax > right[y]) {
+                    right[y] = ax;
+                }
+            }
+        } else {
+            //horizontal
+            for(i64 x = ax; x <= bx; x++) {
+                if(ay < top[x]) {
+                    top[x] = ay;
+                }
+
+                if(ay > bottom[x]) {
+                    bottom[x] = ay;
+                }
+            }
+        }
+    }
+    delete p3timing;
+
+
+
+    for(u64 a = 0; a < numcount-1; a++) {
         for(u64 b = a+1; b < numcount; b++) {
             i64 ax = nums[a*2];
             i64 ay = nums[(a*2)+1];
             i64 bx = nums[b*2];
             i64 by = nums[(b*2)+1];
-
-            if(ax < bx) {
-                i64 tmp = bx;
-                bx = ax;
-                bx = tmp;
-            }
-
-            if(ay < by) {
-                i64 tmp = by;
-                by = ay;
-                by = tmp;
-            }
-
-            bool areaok = true;
-
-            // left - vertical check are odd numbers
-            for(u64 c = 1; c <= numcount; c+=2) {
-                u64 d = (c+1) % numcount;
-                i64 cx = nums[c*2];
-                i64 cy = nums[(c*2)+1];
-                i64 dy = nums[(d*2)+1];
+            bool goodarea = true;
 
 
-            }
+            area = (abs(ax-bx)+1) * (abs(ay-by)+1); // +1 because numbers are inclusive
+            if(area > res) {
+                // checking only potential larger areas
 
+                if(ax > bx) {
+                    i64 tmp = bx;
+                    bx = ax;
+                    ax = tmp;
+                }
 
-            if(areaok) {
-                area = (abs(ax-bx)+1) * (abs(ay-by)+1); // +1 because numbers are inclusive
-                if(area > res) {
+                if(ay > by) {
+                    i64 tmp = by;
+                    by = ay;
+                    ay = tmp;
+                }
+
+                //vertical
+                for(i64 y = ay; y <= by; y++) {
+                    if(left[y] > ax) {
+                        goodarea = false;
+                        break;
+                    }
+
+                    if(right[y] < bx) {
+                        goodarea = false;
+                        break;
+                    }
+                }
+
+                if(!goodarea) {
+                    continue;
+                }
+
+                //horizontal
+                for(i64 x = ax; x <= bx; x++) {
+                    if(top[x] > ay) {
+                        goodarea = false;
+                        break;
+                    }
+
+                    if(bottom[x] < by) {
+                        goodarea = false;
+                        break;
+                    }
+                }
+
+                if(goodarea) {
                     res = area;
                 }
             }
-
-            // we need to check from every side
-
-            // if(ax > maxx) {
-            //     maxx = ax;
-            // }
-
-            // if(ay > maxy) {
-            //     maxy = ay;
-            // }
         }
     }
 
 
-    // // Start by creating the perimetter of the shape
-    // i64* perimdata = new i64[2*(maxx+maxy)];
-    // i64* lef = perimdata;
-    // i64* rig = lef + maxy;
-    // i64* top = rig + maxy;
-    // i64* bot = top + maxx;
 
-    // for(u64 y = 0; y < maxy; y++) {
-    //     lef[y] = 0;
-    //     rig[y] = maxx;
-    // }
-    
-    // for(u64 x = 0; x < maxx; x++) {
-    //     top[x] = 0;
-    //     bot[x] = maxy;
-    // }
-
-    // u64 b;
-    // for(u64 a = 0; a < numcount; a++) {
-    //     b = a+1;
-    //     if(b >= numcount) {
-    //         b = 0;
-    //     }
-
-    //     i64 ax = nums[a*2];
-    //     i64 ay = nums[(a*2)+1];
-    //     i64 bx = nums[b*2];
-    //     i64 by = nums[(b*2)+1];
-
-    //     if(ax == bx) {
-    //         //vertical
-    //         if(by < ay) {
-    //             i64 tmp = by;
-    //             by = ay;
-    //             ay = tmp;
-    //         }
-
-    //         for(u64 y = ay; y <= by; y++) {
-    //             if(ax > lef[y]) {
-    //                 lef[y] = ax;
-    //             }
-
-    //             if(ax < rig[y]) {
-    //                 rig[y] = ax;
-    //             }
-    //         }
-    //     } else {
-    //         //horizontal
-    //         if(bx < ax) {
-    //             i64 tmp = bx;
-    //             bx = ax;
-    //             ax = tmp;
-    //         }
-
-    //         for(u64 x = ax; x <= bx; x++) {
-    //             if(ay > top[x]) {
-    //                 top[x] = ay;
-    //             }
-
-    //             if(ay < bot[x]) {
-    //                 bot[x] = ay;
-    //             }
-    //         }
-    //     }
-    // }
-
-
-    delete[] perimdata;
     cout << "Result 2 : " << res << std::endl;
     delete p2timing;
 
